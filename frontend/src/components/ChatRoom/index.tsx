a89f95c8543648a928a1c8f8e7310c0749cd6893
yuda1124 <yuda1124@naver.com>
2021-11-02 23:15:18 +0900

    [fe] feat: antd 설치 및 컴포넌트 변경

--- frontend/src/components/ChatRoom/index.tsx
+++ frontend/src/components/ChatRoom/index.tsx
@@ -1,4 +1,5 @@
 import react, { useState } from 'react';
+import { Layout, Button, Input } from 'antd';
 import style from './index.module.scss';
 
 type Message = {
@@ -6,28 +7,37 @@ type Message = {
   from: number;
 };
 
+const { Header, Content } = Layout;
+
 const ChatRoom = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [text, setText] = useState<string>('');
+
+  const submitMessage = () => {
+    if (text.length === 0) return;
+    setMessages([...messages, { text, from: 0 }]);
+    setText('');
+  };
+
   return (
-    <div className={style.container}>
-      <header>채팅방</header>
-      <ul>
-        {messages.map(msg => {
-          return <li>{msg.text}</li>;
-        })}
-      </ul>
-      <div>
-        <input value={text} onChange={e => setText(e.target.value)}></input>
-        <button
-          onClick={() => {
-            setMessages([...messages, { text, from: 0 }]);
-            setText('');
-          }}>
-          전송
-        </button>
-      </div>
-    </div>
+    <Layout className={style.container}>
+      <Header className={style.header}>
+        <h3>채팅방</h3>
+      </Header>
+      <Content className={style.content}>
+        <ul className={style.messages}>
+          {messages.map(msg => {
+            return <li>{msg.text}</li>;
+          })}
+        </ul>
+        <div className={style.userInput}>
+          <Input value={text} onChange={e => setText(e.target.value)} onPressEnter={submitMessage}></Input>
+          <Button type="primary" onClick={submitMessage} disabled={text.length === 0}>
+            전송
+          </Button>
+        </div>
+      </Content>
+    </Layout>
   );
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/base.scss';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
