c291a463bac2be5afd8f8d67abe5423a06e3e6c6
yuda1124 <yuda1124@naver.com>
2021-11-08 21:38:54 +0900

    [fe] feat: date 스트링을 (오전/오후) hh:mm 포멧으로 변경하는 유틸함수 작성

---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,11 @@
+const formatTime = (dateStr: string) => {
+  const date = new Date(dateStr);
+  let hour = date.getHours();
+  const pre = hour < 12 ? '오전' : '오후';
+  if (hour === 0) hour = 12;
+  else if (hour > 12) hour -= 12;
+  const min = date.getMinutes();
+  return `${pre} ${hour < 10 ? `0${hour}` : hour}:${min < 10 ? `0${min}` : min}`;
+};
+
+export { formatTime };
