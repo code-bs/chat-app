f926adf57f7f16113be7a8cf7bce0adfd374e246
yuda1124 <yuda1124@naver.com>
2021-11-10 00:53:21 +0900

    [fe] feat: header 컴포넌트와 auth context 연결
    - 현재는 userId만 string으로 받고 있지만 추후 변경예정

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Button } from 'antd';
+import { useAuthState, useAuthDispatch, signIn } from '../../contexts';
 import style from './index.module.scss';
 
 const Header = () => {
+  const { userId } = useAuthState();
+  const authDispatch = useAuthDispatch();
+  const onClickSignin = () => {
+    const id = prompt('아이디를 입력하세요');
+    signIn(authDispatch, id as string);
+  };
   return (
     <div className={style.container}>
       <h1 className={style.logo}>Chat App</h1>
       <div className={style.wrap_btn}>
-        <Button type="primary">Sign in</Button>
+        {userId ? (
+          <span className={style.username}>{userId} 님</span>
+        ) : (
+          <Button type="primary" onClick={onClickSignin}>
+            Sign in
+          </Button>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -1,3 +1,4 @@
 export * from './ChatRoom';
 export * from './SpeechBubble';
 export * from './ChatList';
+export * from './Header';
