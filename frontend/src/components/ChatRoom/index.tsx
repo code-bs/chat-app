04e140a99576a2e2ef9390f4a3f988baa16b9a7f
yuda1124 <yuda1124@naver.com>
2021-11-16 23:08:19 +0900

    [fe] feat: context와 ChatRoom 컴포넌트 연결 및 선택되지 않았을 때 UI 표현 추가

--- frontend/src/components/ChatRoom/index.tsx
+++ frontend/src/components/ChatRoom/index.tsx
@@ -3,27 +3,36 @@ import { Layout, Button, Input } from 'antd';
 import { SpeechBubble } from '..';
 import { ChatLog } from '../../types';
 import style from './index.module.scss';
-
+import { useChatState, useChatDispatch, selectRoom } from '../../contexts';
+import { Empty } from 'antd';
 const { Header, Content } = Layout;
 
 const ChatRoom = () => {
   const [messages, setMessages] = useState<ChatLog[]>([]);
   const [text, setText] = useState<string>('');
-
+  const { rooms, selectedRoomId } = useChatState();
+  const chatDispatch = useChatDispatch();
+  const selectedRoom = rooms.find(room => room._id === selectedRoomId);
   const submitMessage = () => {
     if (text.length === 0) return;
     setMessages([...messages, { message: text, userId: 'me', regDate: new Date().toString() }]);
     setText('');
   };
-
+  if (!selectedRoom) {
+    return (
+      <Layout className={style.container}>
+        <Empty description={'Please select chat room!'} />
+      </Layout>
+    );
+  }
   return (
     <Layout className={style.container}>
       <Header className={style.header}>
-        <h3>채팅방</h3>
+        <h3>{selectedRoom.roomName}</h3>
       </Header>
       <Content className={style.content}>
         <ul className={style.messages}>
-          {messages.map(chatLog => {
+          {selectedRoom.chatHistory.map(chatLog => {
             return <SpeechBubble chatLog={chatLog} key={`${chatLog.regDate}`} />;
           })}
         </ul>
