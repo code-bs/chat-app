9cc877ebadda20be3c4ac94478c94db5dcbab3fa
hyunwoo045 <hyunwoo045@gmail.com>
2021-12-21 12:08:35 +0900

    [bs] mysql db 구축 코드

---
+++ backend/setup/schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE bs_auth;
+
+USE bs_auth;
+
+DROP TABLE IF EXISTS tbl_member;
+CREATE TABLE `tbl_member` (
+  userSeqno int(11) NOT NULL AUTO_INCREMENT,
+  userId varchar(20) NOT NULL,
+  password varchar(255) NOT NULL,
+  nickName varchar(10) NOT NULL,
+  register_dt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_dt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (userSeqno)
+) CHARSET=UTF8MB4;
+
+DROP TABLE IF EXISTS tbl_map_friend;
+CREATE TABLE tbl_map_friend (
+  seqno int(11) NOT NULL AUTO_INCREMENT,
+  userSeqno int(11) NOT NULL,
+  friendSeqno int(11) NOT NULL,
+  PRIMARY KEY (seqno),
+  FOREIGN KEY (userSeqno)
+  REFERENCES tbl_member (userSeqno)
+  ON DELETE CASCADE
+) CHARSET=UTF8MB4;
+
+DROP TABLE IF EXISTS tbl_map_room;
+CREATE TABLE tbl_map_room (
+  seqno int(11) NOT NULL AUTO_INCREMENT,
+  userSeqno int(11) NOT NULL,
+  roomSeqno varchar(255) NOT NULL,
+  PRIMARY KEY (seqno),
+  FOREIGN KEY (userSeqno)
+  REFERENCES tbl_member (userSeqno)
+  ON DELETE CASCADE
+) CHARSET=UTF8MB4;
