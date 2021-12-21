057d1fcb884626a0d7db3ad79bd79e7409ba97d9
hyunwoo045 <hyunwoo045@gmail.com>
2021-12-21 11:54:57 +0900

    서버 구동 직후 환경 변수 설정하도록 수정

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,6 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../env/local.env") });
+
 const httpServer = require("./config/initializer/httpServer.js");
 const mongodb = require("./config/initializer/mongodb");
 const websocket = require("./config/initializer/websocket");
