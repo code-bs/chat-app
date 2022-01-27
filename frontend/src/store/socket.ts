3c043778f7bac3ea58444f2e44c759e394b28337
yuda1124 <yuda1124@naver.com>
2022-01-27 22:19:43 +0900

    [fe] feat: sendMessage 함수 및 디버깅 용 함수 작성

--- frontend/src/store/socket.ts
+++ frontend/src/store/socket.ts
@@ -3,4 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const socket = io(`ws://${process.env.REACT_APP_SOCKET_END_POINT}`);
+
+export const sendMessage = <T>(eventType: string, message: T) => {
+  socket.emit(eventType, message);
+};
+
+socket.on('connect', () => {
+  console.log('connect');
+});
+socket.on('disconnect', () => {
+  console.log('disconnect');
+});
+socket.onAny((eventName, ...args) => {
+  console.log(eventName, args);
+});
 export default socket;
