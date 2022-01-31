188b6217897ef41c586b6dd337e2a1c34eba4605
yuda1124 <yuda1124@naver.com>
2022-01-31 14:38:44 +0900

    [fe] feat: user API 통신 함수에 return type 추가

--- frontend/src/apis/user.ts
+++ frontend/src/apis/user.ts
@@ -1,25 +1,34 @@
 import endpoint from './endpoint';
-import { FindUserParams, GetFriendListParams, CheckRequestParams, AddFriendParams } from '../types';
+import {
+  FindUserParams,
+  GetFriendListParams,
+  CheckRequestParams,
+  AddFriendParams,
+  FindUserResponse,
+  GetChatRoomListResponse,
+  CheckRequestResponse,
+  GetFriendListResponse,
+} from '../types';
 
-const findUser = async ({ userId }: FindUserParams) => {
+const findUser = async ({ userId }: FindUserParams): Promise<FindUserResponse> => {
   const { data } = await endpoint.get(`/user/${userId}`);
   console.log(data);
   return data;
 };
 
-const getFriendList = async ({ userId }: GetFriendListParams) => {
+const getFriendList = async ({ userId }: GetFriendListParams): Promise<GetFriendListResponse> => {
   const { data } = await endpoint.get(`/user/friend/${userId}`);
   console.log(data);
   return data;
 };
 
-const checkRequest = async ({ userId }: CheckRequestParams) => {
+const checkRequest = async ({ userId }: CheckRequestParams): Promise<CheckRequestResponse> => {
   const { data } = await endpoint.get(`/user/friend_req/${userId}`);
   console.log(data);
   return data;
 };
 
-const addFriend = async ({ userId, friendId }: AddFriendParams) => {
+const addFriend = async ({ userId, friendId }: AddFriendParams): Promise<void> => {
   await endpoint.post('/auth', { userId, friendId });
 };
 
