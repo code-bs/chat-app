28306a54272abc5e15b6993863f7491777cd60eb
yuda1124 <yuda1124@naver.com>
2022-02-02 20:57:33 +0900

    [fe] feat: 자동 로그인 login 추가

--- frontend/src/router/RequireAuth.tsx
+++ frontend/src/router/RequireAuth.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, useLocation, Outlet } from 'react-router-dom';
-import { useAppSelector } from '../store/hooks';
+import { useAppSelector, useAppDispatch } from '../store/hooks';
+import { getRefreshTokenAsync } from '../store/auth/actions';
 
 const RequireAuth = () => {
   const auth = useAppSelector(state => state.auth);
   const location = useLocation();
+  const dispatch = useAppDispatch();
   const {
     signin: { data },
   } = auth;
+  useEffect(() => {
+    if (!data) dispatch(getRefreshTokenAsync.request({}));
+  }, [dispatch, data]);
+
   if (!data) {
     return <Navigate to="/signin" state={{ from: location }} />;
   }
